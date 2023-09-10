2ff84ed16b3ec05d44ed1f5d95a0615b8ca2bc7a
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-10 21:59:39 +0900

    second commit

---
+++ test/test_foo.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require '../lib/foo'
+
+class TC_Foo < Test::Unit::TestCase
+  def setup
+    @obj = Foo.new
+  end
+
+  # def teardown
+  # end
+
+  def test_foo
+    assert_equal("foo", @obj.foo)
+  end
+  def test_bar
+    assert_equal("bar", @obj.bar)
+  end
+end
