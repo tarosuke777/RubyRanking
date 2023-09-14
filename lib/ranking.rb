db589e5584f541f62cac8f11af0ddfdc814345cd
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-14 18:11:19 +0900

    add csvデータ

---
+++ lib/ranking.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+def ranking
+  entry_log = File.read('../testdata/ranking/in/game_ently_log.csv')
+  puts entry_log
+end
--- test/test_foo.rb
+++ test/test_foo.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require '../lib/main'
+require '../lib/ranking'
 
 class TcMain < MiniTest::Test
-  def setup
-    @obj = Main.new
-  end
-
-  def teardown
-  end
-
-  def test_foo
+  def test_ranking
     out = File.read('../testdata/ranking/out/success.csv')
-    assert_output(out) { @obj.foo }
-  end
-
-  def test_bar
-    assert_equal('bar', @obj.bar)
+    assert_output(out) { ranking }
   end
 end
