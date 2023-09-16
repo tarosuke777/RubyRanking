64791e8b1d2acd582dd70b500fea3635703e0b4b
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-16 21:55:44 +0900

    エントリーログをハッシュ化

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -2,6 +2,16 @@
 
 def ranking
   dir = File.expand_path('..', __dir__)
-  entry_log = File.read("#{dir}/testdata/ranking/in/game_ently_log.csv")
-  puts entry_log
+  entry_log = File.open("#{dir}/testdata/ranking/in/game_ently_log.csv")
+
+  entry_id_to_name = {}
+  entry_log.each_line do |line|
+    values = line.split(',')
+    entry_id_to_name.store(values[0], values[1])
+  end
+
+  puts entry_id_to_name
+  score_log = File.read("#{dir}/testdata/ranking/in/game_score_log.csv")
 end
+
+ranking
