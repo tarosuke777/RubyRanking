caaf293d9c2c6f63e0c3ad6a26ab09c65be567ec
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-17 20:09:27 +0900

    ソート処理を追加

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -5,13 +5,23 @@ require 'csv'
 def ranking
   dir = File.expand_path('..', __dir__)
 
-  entry_id_to_name = {}
+  player_id_to_name = {}
   CSV.foreach("#{dir}/testdata/ranking/in/game_ently_log.csv", headers: true) do |row|
-    entry_id_to_name.store(row['player_id'], row['handle_name'])
+    player_id_to_name.store(row['player_id'], row['handle_name'])
   end
 
-  puts entry_id_to_name
-  score_log = File.read("#{dir}/testdata/ranking/in/game_score_log.csv")
+  player_id_to_score = {}
+  CSV.foreach("#{dir}/testdata/ranking/in/game_score_log.csv", headers: true) do |row|
+    if player_id_to_score.key(row['player_id'])
+      score = player_id_to_score.fetch(row['player_id'])
+      player_id_to_score.store(row['player_id'], row['score']) if score < row['score'].to_i
+    else
+      player_id_to_score.store(row['player_id'], row['score'].to_i)
+    end
+  end
+
+  player_id_to_score_sorted = player_id_to_score.sort_by { |_, v| -v }.to_h
+  p player_id_to_score_sorted
 end
 
 ranking
