117dd2e3e9d2adc392f7af2d57a5ed54588a1269
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-19 22:36:50 +0900

    ファイル名を実行時に引数より取得するように変更

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -2,51 +2,47 @@
 
 require 'csv'
 
-def ranking
-  dir = File.expand_path('..', __dir__)
+dir = File.expand_path('..', __dir__)
 
-  player_id_to_name = {}
-  CSV.foreach("#{dir}/testdata/ranking/in/game_ently_log.csv", headers: true) do |row|
-    player_id_to_name.store(row['player_id'], row['handle_name'])
-  end
+player_id_to_name = {}
+CSV.foreach("#{dir}/#{ARGV[0]}", headers: true) do |row|
+  player_id_to_name.store(row['player_id'], row['handle_name'])
+end
 
-  player_id_to_score = {}
-  CSV.foreach("#{dir}/testdata/ranking/in/game_score_log.csv", headers: true) do |row|
-    if player_id_to_score.key(row['player_id'])
-      score = player_id_to_score.fetch(row['player_id'])
-      player_id_to_score.store(row['player_id'], row['score']) if score < row['score'].to_i
-    else
-      player_id_to_score.store(row['player_id'], row['score'].to_i)
-    end
-  end
-
-  player_id_to_score_sorted = player_id_to_score.sort_by { |_, v| -v }.to_h
-
-  prev_score = 0
-  rank = 0
-  out_rank = 0
-  ranking_data = []
-
-  player_id_to_score_sorted.each do |key, value|
-    player_id = key
-    handle_name = player_id_to_score[player_id]
-    score = value
-
-    next if handle_name == ''
-
-    rank += 1
-
-    out_rank = rank if score != prev_score
-
-    break if out_rank > 10
-
-    ranking_data.push("#{out_rank},#{player_id},#{handle_name},#{score}")
-    prev_score = score
-  end
-
-  ranking_data.each do |value|
-    p value
+player_id_to_score = {}
+CSV.foreach("#{dir}/#{ARGV[1]}", headers: true) do |row|
+  if player_id_to_score.key(row['player_id'])
+    score = player_id_to_score.fetch(row['player_id'])
+    player_id_to_score.store(row['player_id'], row['score']) if score < row['score'].to_i
+  else
+    player_id_to_score.store(row['player_id'], row['score'].to_i)
   end
 end
 
-ranking
+player_id_to_score_sorted = player_id_to_score.sort_by { |_, v| -v }.to_h
+
+prev_score = 0
+rank = 0
+out_rank = 0
+ranking_data = []
+
+player_id_to_score_sorted.each do |key, value|
+  player_id = key
+  handle_name = player_id_to_score[player_id]
+  score = value
+
+  next if handle_name == ''
+
+  rank += 1
+
+  out_rank = rank if score != prev_score
+
+  break if out_rank > 10
+
+  ranking_data.push("#{out_rank},#{player_id},#{handle_name},#{score}")
+  prev_score = score
+end
+
+ranking_data.each do |value|
+  p value
+end
