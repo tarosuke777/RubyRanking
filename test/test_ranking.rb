e8adc86eda560ff8a114f79c4565c64fd404a075
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2023-09-25 15:46:03 +0900

    テストケースを追加

--- test/test_ranking.rb
+++ test/test_ranking.rb
@@ -11,4 +11,13 @@ class TcMain < MiniTest::Test
     args = ['testdata/ranking/in/game_ently_log.csv', 'testdata/ranking/in/game_score_log.csv']
     assert_output(out.read) { main(args) }
   end
+
+  def test_main_none
+    dir = File.expand_path('..', __dir__)
+    out = File.open("#{dir}/testdata/ranking/out/none.csv")
+
+    args = ['testdata/ranking/in/game_ently_log_none.csv', 'testdata/ranking/in/game_score_log_none.csv']
+    assert_output(out.read) { main(args) }
+
+  end
 end
